072fa636e5c8cf4d304795e555ab81dbaccd33bf
heroichornet <schnipper@gmail.com>
2022-10-03 23:51:00 +0200

    improving new test

--- testsuite/src/gn_i2c.rs
+++ testsuite/src/gn_i2c.rs
@@ -5,7 +5,7 @@ use defmt::unwrap;
 use defmt_rtt as _; // global logger
 use nucleo_wl55jc_bsp::hal::{
     cortex_m::{self, delay::Delay},
-    embedded_hal::blocking::i2c::{WriteRead,Write},
+    embedded_hal::blocking::i2c::{WriteRead,Write,Read},
     gpio::{pins, Output, PinState,PortB,PortA},
     i2c::{I2c1},
     pac::{self},
@@ -20,8 +20,13 @@ const I2C_FREQUENCY: u32 = 100_000;
 mod tests {
     use super::*;
 
+    struct TestArgs {
+        i2c: I2c1<(pins::A9, pins::A10)>,
+        delay: Delay,
+    }
+
     #[init]
-    fn init() -> I2c1<(pins::A9, pins::A10)> {
+    fn init() -> TestArgs {
         cortex_m::interrupt::free(|cs| {
             let mut dp: pac::Peripherals = unwrap!(pac::Peripherals::take());
             let cp: pac::CorePeripherals = defmt::unwrap!(pac::CorePeripherals::take());
@@ -36,43 +41,66 @@ mod tests {
 
             // Power the sensor
             sensor_power_enable.set_level(PinState::High);
-            let mut delay: Delay = new_delay(cp.SYST, &dp.RCC);
-            // wait for the sensor to be powered up
 
+            // wait for the sensor to be powered up
+            let mut delay: Delay = new_delay(cp.SYST, &dp.RCC);
             delay.delay_us(240);
 
-            I2c1::new(
+            let i2c = I2c1::new(
                 dp.I2C1,
                 (gpioa.a9, gpioa.a10),
                 I2C_FREQUENCY,
                 &mut dp.RCC,
                 false,
                 cs,
-            )
+                );
+
+            TestArgs{
+                i2c,
+                delay
+            }
+
         })
     }
 
 
     #[test]
-    fn shtc3_measurement(i2c: &mut I2c1<(pins::A9, pins::A10)>) {
+    fn shtc3_measurement(ta: &mut TestArgs) {
         defmt::warn!("A SHTC3 sensor must be connected to the board on pins A9 (SCL) & A10 (SDA) for this test to work");
-        let mut cmd: [u8; 2] = [0x35, 0x17];
-        let mut response: [u8; 3] = [0; 3];
 
-        let result = i2c.write(0x70, &cmd);
+        let mut response: [u8; 6] = [0; 6];
+
+        // send wake up command
+        let result = ta.i2c.write(0x70, &[0x35, 0x17]);
 
         match result {
             Ok(()) => defmt::info!("SHTC3 was woken up"),
             Err(e) => defmt::error!("I2C error: {}", e),
         }
 
-        cmd = [0xEF, 0xC8];
-
-        let result = i2c.write_read(0x70, &cmd, &mut response);
+        // get sensor ID
+        let result = ta.i2c.write_read(0x70, &[0xEF, 0xC8], &mut response);
 
         match result {
             Ok(()) => defmt::info!("SHTC3 ID = {:x} ",response),
             Err(e) => defmt::error!("I2C error: {}", e),
+        }
+
+        for _ in 1..10 {
+            // get measurement read relative humditiy first and enable clock stretching
+            let result = ta.i2c.write_read(0x70, &[0x5C, 0x24],&mut response);
+
+            match result {
+                Ok(()) => {
+                    defmt::info!("response:{:x}",response);
+                    let relative_humidity = 100.0*((response[1] as u16 + ((response[0] as u16) << 8)) as f32)/65535.0;
+                    let temperature = ((response[4] as u16 + ((response[3] as u16) << 8)) as f32)*175.0/65535.0 - 45.0;
+                    defmt::info!("rh {}%", relative_humidity);
+                    defmt::info!("temp {}°C", temperature);
+                },
+                Err(e) => defmt::error!("I2C error: {}", e),
+            }
+            ta.delay.delay_ms(100);
         }
     }
 
